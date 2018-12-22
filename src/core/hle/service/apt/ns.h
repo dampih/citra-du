af6824b2687896a81cbf4b421ce0a36680c8ff12
citrabot <citra@citra-emu.org>
2018-12-22 02:36:48 +0000

    Merge PR 4472

--- src/core/hle/service/apt/ns.h
+++ src/core/hle/service/apt/ns.h
@@ -17,7 +17,4 @@ namespace Service::NS {
 /// Loads and launches the title identified by title_id in the specified media type.
 Kernel::SharedPtr<Kernel::Process> LaunchTitle(FS::MediaType media_type, u64 title_id);
 
-/// Registers all NS services with the specified service manager.
-void InstallInterfaces(Core::System& system);
-
 } // namespace Service::NS
